ca2fc768729f7cf7d87b100816265a817586637f
cplincoln <122641181+cplincoln@users.noreply.github.com>
2023-01-13 15:41:35 -0500

    Update Price_History.py

--- Price_History.py
+++ Price_History.py
@@ -1,20 +1,21 @@
 # Version 3.6.1    
 import requests, config, pprint
 
-spy_history_header = {'Authorization': 'Bearer {}'.format(config.access_token), 'Accept': 'application/json'}
-spy_history_url = '{}/v1/markets/timesales'.format(config.base_url)
+history_header = {'Authorization': 'Bearer {}'.format(config.access_token), 'Accept': 'application/json'}
+history_url = '{}/v1/markets/timesales'.format(config.base_url)
 
-weekStart = '2023-01-09 09:30'
-weekEnd = '2023-01-13 16:00'
+dateStart = '2023-01-09 09:30'
+dateEnd = '2023-01-13 16:00'
 
 symbol = 'SPY'
+interval = '5min'
 
-spy_history_params={'symbol': symbol, 'interval': '5min', 'start': weekStart, 'end': weekEnd, 'session_filter': 'open'}
+history_params={'symbol': symbol, 'interval': interval, 'start': dateStart, 'end': dateEnd, 'session_filter': 'open'}
 
 response = requests.get(
-    spy_history_url,
-    params= spy_history_params,
-    headers= spy_history_header
+    history_url,
+    params= history_params,
+    headers= history_header
 )
 
 json_response = response.json()
